e8d2e5198198b8bcf1a59bb4e8825f55e895dd06
xkendx <63884610+xkendx@users.noreply.github.com>
2020-11-15 21:54:56 +0300

    update

--- imdb/src/components/CardList/index.js
+++ imdb/src/components/CardList/index.js
@@ -1,8 +1,10 @@
+import {useContext} from 'react';
 import {Card} from "../Card";
+import {MovieContext} from "../../Movie";
 import {StyledCardList} from "./CardList.style"
 
-export const CardList = ({movieList, baseImageUrl}) => {
-  
+export const CardList = () => {
+  const {movieList, baseImageUrl} = useContext(MovieContext);
     return (
         <StyledCardList>
            {movieList?.map(
@@ -10,6 +12,7 @@ export const CardList = ({movieList, baseImageUrl}) => {
                <Card 
                 key={index} 
                 title={movie.title}
+                id={movie.id}
                 imgSrc={baseImageUrl + movie.poster_path}
                 />)
             )}
