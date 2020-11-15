bd2535aab5fe7b9797dc62b17bc7ca5ba00b78b1
xkendx <63884610+xkendx@users.noreply.github.com>
2020-11-15 18:04:42 +0300

    update

--- session-02/src/components/CardList/index.js
+++ session-02/src/components/CardList/index.js
@@ -1,16 +1,18 @@
-import { Card } from "../Card";
-import { StyledCardList } from "./CardList.style";
+import {Card} from "../Card";
+import {StyledCardList} from "./CardList.style"
 
-export const CardList = ({ movieList, baseImageUrl }) => {
-  return (
-    <StyledCardList>
-      {movieList?.map((movie, index) => (
-        <Card
-          key={index}
-          title={movie.title}
-          imgSrc={baseImageUrl + movie.poster_path}
-        />
-      ))}
-    </StyledCardList>
-  );
-};
+export const CardList = ({movieList, baseImageUrl}) => {
+  
+    return (
+        <StyledCardList>
+           {movieList?.map(
+               (movie, index) => (
+               <Card 
+                key={index} 
+                title={movie.title}
+                imgSrc={baseImageUrl + movie.poster_path}
+                />)
+            )}
+        </StyledCardList>
+    )
+}
